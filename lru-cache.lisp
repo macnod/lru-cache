2f3666df722aa4788d06fbc990ac7413b60e7750
Donnie Cameron <macnod@gmail.com>
2025-12-31 06:20:00 -0800

    Added cache-remove and cache-usage (#2)
    * Added cache-remove and cache-usage
    
    * Added test to check return value of cache-remove.
    
    * Addressed code-review.

--- lru-cache.lisp
+++ lru-cache.lisp
@@ -15,7 +15,7 @@
       :initform (make-instance 'dl:dlist)
       :reader cache-list
       :documentation "Doubly-linked list maintaining LRU order")
-    (max-size
+    (cache-max-size
       :initarg :max-size
       :initform 100
       :reader cache-max-size
@@ -36,9 +36,15 @@
                           :test (test-function cache)
                           :size (1+ (cache-max-size cache)))))
 
-(defun cache-size (cache)
+(defmethod cache-size ((cache lru-cache))
   "Return the current number of entries in the cache."
   (dl:len (cache-list cache)))
+
+(defmethod cache-usage ((cache lru-cache))
+  "Return the usage ratio of the cache (current size / max size)."
+  (if (zerop (cache-max-size cache))
+    0.0
+    (/ (float (cache-size cache)) (cache-max-size cache))))
 
 (defmethod cache-put (key value (cache lru-cache))
   "Insert or update a key-value pair in the cache. If the key exists, update its
@@ -79,3 +85,15 @@ moves the entry to the front (most recent)."
           (setf (gethash key cache-table) (dl:head cache-list))
           (values value t))
         (values nil nil)))))
+
+(defmethod cache-remove (key (cache lru-cache))
+  "Remove a key-value pair from the cache by key. Returns T if the key was found
+and removed, NIL otherwise."
+  (let ((cache-table (cache-table cache))
+         (cache-list (cache-list cache)))
+    (multiple-value-bind (node found)
+      (gethash key cache-table)
+      (when found
+        (dl:delete-node cache-list node)
+        (remhash key cache-table)
+        t))))
